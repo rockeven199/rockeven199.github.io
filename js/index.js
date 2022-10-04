cb4d5c7a4f80942a476ac4244e96dfdf7f2d685c
rockeven199 <67353571+rockeven199@users.noreply.github.com>
2022-10-04 22:13:04 +0800

    submit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-function onEle(onEle) { 
-    console.log(onEle) 
-    onEle.style.cssText="background-image:url('../img/19186sAkacEsSN2.jpg');transition:all 0.5s;"
+function onEle(onEle) {
+    console.log(onEle)
+    onEle.style.cssText = "background-image:url('../img/19186sAkacEsSN2.jpg');transition:all 0.5s;"
 }
 
-function outEle(outEle) {  
-    outEle.style.cssText="url(../img/OIP-C.jfif);transition:all 0.5s;"
+function outEle(outEle) {
+    outEle.style.cssText = "url(../img/OIP-C.jfif);transition:all 0.5s;"
 
 }

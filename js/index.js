6cb19bf46d5cbc2966cc43c9110ad1b7c63b5e4d
rockeven199 <67353571+rockeven199@users.noreply.github.com>
2022-10-11 23:30:05 +0800

    submit

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ function outEle(outEle) {
     outEle.style.cssText = "url(../img/OIP-C.jfif);transition:all 0.5s;"
 
 }
+
+
+// aside
+
+$(".nav-menu").click(()=>{
+    $(".header-nav").css("transform","translateX(0)")
+})
+
+//自适应
+
+window.onload = () => {
+    if (($(window).width() >= 360 && $(window).width() <= 500) && ($(window).height() >= 600 && $(window).height() <= 800)) {
+
+    }
+}

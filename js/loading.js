c0a5ef950977d9fc7cf400ef5cd3abdb8971c2a0
rockeven199 <67353571+rockeven199@users.noreply.github.com>
2023-01-25 23:07:59 +0800

    submit

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,20 @@
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         document.querySelector(".loading").style.display = "none";
+        document.querySelector(".prograss-pic").removeAttribute("style");
+        document.querySelector(".loading").style.animation = "loading-complete 1s forward"
+        document.querySelector("body").style.overflow = "";
+        // 临时样式
+        // document.querySelector(".prograss-pic").style.display = "none";
+    }
+    else {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".prograss-pic").style.display = "none";
+        var fontColor = ['#2BD52B', '#E6E61A', '#DD2222', '#2B6FD5', '#CC33CC', '#E6941A'];
+        var realDom = document.querySelectorAll("span");
+
+        realDom.forEach(element => {
+            element.style.color = fontColor[Math.floor(Math.random() * (6 - 1 + 1))];
+        });
     }
 }
